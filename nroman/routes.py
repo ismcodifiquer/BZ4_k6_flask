816017911e14220dc556ff3fcbcd52d828c4e801
ismcodifiquer <60313089+ismcodifiquer@users.noreply.github.com>
2020-02-18 19:40:17 +0100

    Fin de API

--- nroman/routes.py
+++ nroman/routes.py
@@ -1,5 +1,24 @@
 from nroman import app
+from utils.romannumber import RomanNumber
+from flask import jsonify
 
 @app.route("/")
 def index():
     return "Funcionando..."
+
+@app.route("/toroman/<valor>")
+def to_roman(valor):
+    resp = {'correct': False}
+
+    try:
+        valor = int(valor)
+    except:
+        resp['message'] = 'Valor incorrecto'
+        return jsonify(resp)
+
+    nr = RomanNumber(valor)
+    resp['correct'] = True
+    resp['result'] = {'arabigo': valor, 'romano': str(nr)}
+    return jsonify(resp)
+    
+
